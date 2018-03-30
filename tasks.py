e74569295c4b98fe60d55c7a0744467831bb3d94
Malitha H. Kabir <malitha12345@gmail.com>
2018-03-30 21:08:40 +0800

    update
    skip

--- tasks.py
+++ tasks.py
@@ -1,17 +1,19 @@
 from __future__ import absolute_import, unicode_literals
 from .celery import app
-
+from subprocess import PIPE, Popen
 
 @app.task
 def add(x, y):
     return x + y
 
-
 @app.task
-def mul(x, y):
-    return x * y
-
-
-@app.task
-def xsum(numbers):
-    return sum(numbers)
+def run_script(script_path):
+    p = Popen(["python", "-u", script_path],stdout=PIPE)
+    p.wait()
+    out = p.stdout.readlines()
+    filename = script_path.replace('py', 'txt')
+    with open(filename, 'wb') as f:
+        for line in out:
+            f.write(line)
+            
+    return 'done!'
